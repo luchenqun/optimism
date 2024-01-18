decd13d30d3b4bd320529f92a216d5be5cf7ed96
Adrian Sutton <adrian@oplabs.co>
2024-01-18 15:02:06 +1000

    op-challenger: Load proposed large preimages
    Introduces the infrastructure for monitoring large pre-image proposals, up to loading the list of in-progress games from the contract.
    Actual content of the preimages is not yet loaded or checked.

--- op-challenger/game/fault/contracts/oracle.go
+++ op-challenger/game/fault/contracts/oracle.go
@@ -1,11 +1,13 @@
 package contracts
 
 import (
+	"context"
 	"fmt"
 	"math/big"
 
 	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
 	"github.com/ethereum-optimism/optimism/op-challenger/game/fault/types"
+	gameTypes "github.com/ethereum-optimism/optimism/op-challenger/game/types"
 	"github.com/ethereum-optimism/optimism/op-service/sources/batching"
 	"github.com/ethereum-optimism/optimism/op-service/txmgr"
 	"github.com/ethereum/go-ethereum/common"
@@ -13,6 +15,8 @@ import (
 
 const (
 	methodLoadKeccak256PreimagePart = "loadKeccak256PreimagePart"
+	methodProposalCount             = "proposalCount"
+	methodProposals                 = "proposals"
 )
 
 // PreimageOracleContract is a binding that works with contracts implementing the IPreimageOracle interface
@@ -42,4 +46,26 @@ func (c *PreimageOracleContract) Addr() common.Address {
 func (c *PreimageOracleContract) AddGlobalDataTx(data *types.PreimageOracleData) (txmgr.TxCandidate, error) {
 	call := c.contract.Call(methodLoadKeccak256PreimagePart, new(big.Int).SetUint64(uint64(data.OracleOffset)), data.GetPreimageWithoutSize())
 	return call.ToTxCandidate()
+}
+
+func (c *PreimageOracleContract) GetActivePreimages(ctx context.Context, blockHash common.Hash) ([]gameTypes.LargePreimageMetaData, error) {
+	results, err := batching.ReadArray(ctx, c.multiCaller, batching.BlockByHash(blockHash), c.contract.Call(methodProposalCount), func(i *big.Int) *batching.ContractCall {
+		return c.contract.Call(methodProposals, i)
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to load claims: %w", err)
+	}
+
+	var proposals []gameTypes.LargePreimageMetaData
+	for idx, result := range results {
+		proposals = append(proposals, c.decodeProposal(result, idx))
+	}
+	return proposals, nil
+}
+
+func (c *PreimageOracleContract) decodeProposal(result *batching.CallResult, idx int) gameTypes.LargePreimageMetaData {
+	return gameTypes.LargePreimageMetaData{
+		Claimant: result.GetAddress(0),
+		UUID:     result.GetBigInt(1),
+	}
 }
--- op-challenger/game/fault/contracts/oracle_test.go
+++ op-challenger/game/fault/contracts/oracle_test.go
@@ -1,11 +1,13 @@
 package contracts
 
 import (
+	"context"
 	"math/big"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-bindings/bindings"
 	"github.com/ethereum-optimism/optimism/op-challenger/game/fault/types"
+	gameTypes "github.com/ethereum-optimism/optimism/op-challenger/game/types"
 	"github.com/ethereum-optimism/optimism/op-service/sources/batching"
 	batchingTest "github.com/ethereum-optimism/optimism/op-service/sources/batching/test"
 	"github.com/ethereum/go-ethereum/common"
@@ -13,12 +15,7 @@ import (
 )
 
 func TestPreimageOracleContract_LoadKeccak256(t *testing.T) {
-	oracleAbi, err := bindings.PreimageOracleMetaData.GetAbi()
-	require.NoError(t, err)
-
-	stubRpc := batchingTest.NewAbiBasedRpc(t, oracleAddr, oracleAbi)
-	oracleContract, err := NewPreimageOracleContract(oracleAddr, batching.NewMultiCaller(stubRpc, batching.DefaultBatchSize))
-	require.NoError(t, err)
+	stubRpc, oracleContract := setupPreimageOracleTest(t)
 
 	data := &types.PreimageOracleData{
 		OracleKey:    common.Hash{0xcc}.Bytes(),
@@ -33,4 +30,60 @@ func TestPreimageOracleContract_LoadKeccak256(t *testing.T) {
 	tx, err := oracleContract.AddGlobalDataTx(data)
 	require.NoError(t, err)
 	stubRpc.VerifyTxCandidate(tx)
+}
+
+func TestGetActivePreimages(t *testing.T) {
+	stubRpc, oracle := setupPreimageOracleTest(t)
+	blockHash := common.Hash{0xaa}
+	stubRpc.SetResponse(
+		oracleAddr,
+		methodProposalCount,
+		batching.BlockByHash(blockHash),
+		[]interface{}{},
+		[]interface{}{big.NewInt(3)})
+
+	preimage1 := gameTypes.LargePreimageMetaData{
+		Claimant: common.Address{0xaa},
+		UUID:     big.NewInt(1111),
+	}
+	preimage2 := gameTypes.LargePreimageMetaData{
+		Claimant: common.Address{0xbb},
+		UUID:     big.NewInt(2222),
+	}
+	preimage3 := gameTypes.LargePreimageMetaData{
+		Claimant: common.Address{0xcc},
+		UUID:     big.NewInt(3333),
+	}
+	expectGetProposals(stubRpc, batching.BlockByHash(blockHash), preimage1, preimage2, preimage3)
+	preimages, err := oracle.GetActivePreimages(context.Background(), blockHash)
+	require.NoError(t, err)
+	require.Equal(t, []gameTypes.LargePreimageMetaData{preimage1, preimage2, preimage3}, preimages)
+}
+
+func expectGetProposals(stubRpc *batchingTest.AbiBasedRpc, block batching.Block, proposals ...gameTypes.LargePreimageMetaData) {
+	for i, proposal := range proposals {
+		expectGetProposal(stubRpc, block, int64(i), proposal)
+	}
+}
+
+func expectGetProposal(stubRpc *batchingTest.AbiBasedRpc, block batching.Block, idx int64, proposal gameTypes.LargePreimageMetaData) {
+	stubRpc.SetResponse(
+		oracleAddr,
+		methodProposals,
+		block,
+		[]interface{}{big.NewInt(idx)},
+		[]interface{}{
+			proposal.Claimant,
+			proposal.UUID,
+		})
+}
+
+func setupPreimageOracleTest(t *testing.T) (*batchingTest.AbiBasedRpc, *PreimageOracleContract) {
+	oracleAbi, err := bindings.PreimageOracleMetaData.GetAbi()
+	require.NoError(t, err)
+
+	stubRpc := batchingTest.NewAbiBasedRpc(t, oracleAddr, oracleAbi)
+	oracleContract, err := NewPreimageOracleContract(oracleAddr, batching.NewMultiCaller(stubRpc, batching.DefaultBatchSize))
+	require.NoError(t, err)
+	return stubRpc, oracleContract
 }
